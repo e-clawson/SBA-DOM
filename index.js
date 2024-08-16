bbe835711cba196690864096e3ca555ee6cf8eef
Elizabeth Clawson <emclawson1@gmail.com>
2024-08-16 12:52:43 -0500

    fixed heading css

--- index.js
+++ index.js
@@ -8,14 +8,16 @@ let navBarTabs = [
 
 //selected title for page to ensure js was being read by index.html
 //sert it's backgronud to light blue 
-let mainEl = document.querySelector("main")
-mainEl.style.backgroundColor = "lightblue";
+let title = document.querySelector("h1")
+title.style.backgroundColor = "lightblue";
+title.classList.add("flex-ctr");
+console.log(title)
 
 //created a nav element, created a query for the header, 
 // and appended the navBar to the header element 
 let navBar = document.createElement("nav")
 let headerEl = document.querySelector("header")
 headerEl.appendChild(navBar);
-
+navBar.classList.add("flex-around")
 
 // navBarTabs.forEach
