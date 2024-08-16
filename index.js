af92d6d8e90f782f9744f59e9bb7d63e4e844ba3
Elizabeth Clawson <emclawson1@gmail.com>
2024-08-16 11:50:06 -0500

    added navBar to html using js

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
+//created navBar tabs data to populate the NavBer 
+let navBarTabs = [
+    { text: "home", href: "/home" },
+    { text: "about", href: "/about" },
+    { text: "exhibits", href: "/exhibits" },
+    { text: "contact", href: "/contact" },
+]
 
+//selected title for page to ensure js was being read by index.html
+//sert it's backgronud to light blue 
 let mainEl = document.querySelector("main")
-console.log(mainEl)
 mainEl.style.backgroundColor = "lightblue";
+
+//created a nav element, created a query for the header, 
+// and appended the navBar to the header element 
+let navBar = document.createElement("nav")
+let headerEl = document.querySelector("header")
+headerEl.appendChild(navBar);
+
+
+// navBarTabs.forEach
